cf0ba01b5da336108faaa3c8cb4b7393ce984db5
highjump0615 <lixiang901112@163.com>
2017-08-09 22:55:40 +0800

    Removed sensitive info

--- config/config.js
+++ config/config.js
@@ -1,7 +1,4 @@
 module.exports = {
-    appid: 'wx56dcb601dac325b4',
-    secret: '16ab8277bb0ca5b3a86161f932f421c1',
-
     baseUrl: 'https://tjyx.weifengkeji.top/',
     api: {
         baseUrl: 'https://tjyx.weifengkeji.top/api/v1'
