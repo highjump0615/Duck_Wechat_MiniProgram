3c84a947f5893b4464f5056de5b5e1b81ad733f6
highjump0615 <lixiang901112@163.com>
2017-08-09 15:49:03 +0800

    Updated backend url

--- config/config.js
+++ config/config.js
@@ -2,9 +2,9 @@ module.exports = {
     appid: 'wx56dcb601dac325b4',
     secret: '16ab8277bb0ca5b3a86161f932f421c1',
 
-    baseUrl: 'https://hly.weifengkeji.top/public/',
+    baseUrl: 'https://tjyx.weifengkeji.top/',
     api: {
-        baseUrl: 'https://hly.weifengkeji.top/public/api/v1'
+        baseUrl: 'https://tjyx.weifengkeji.top/api/v1'
     },
 
     /**
